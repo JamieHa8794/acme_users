40d49cc9b5c3a4469e1c5f3eef9770a17d3a1446
JamieHa8794 <JamieHa8794@gmail.com>
2022-08-25 23:01:59 -0400

    addeded styles

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const { db, syncAndSeed, models: {User} } = require('./db')
 const express = require('express');
 const app = express();
+const path = require('path')
 
+app.get('/styles.css', (req, res) => res.sendFile(path.join(__dirname,'styles.css')));
 
-app.get('/', async (req, res, next)=>{
+app.get('/', (req, res, next)=>{
+    try{
+        res.redirect('/users')
+    }
+    catch(err){
+        next(err);
+    }
+})
+app.get('/users', async (req, res, next)=>{
     try{
         const users = await User.findAll();
-        res.send(users);
+        res.send(`
+            <html>
+            <head>
+            <link rel='stylesheet' href='/styles.css'/>
+            </head>
+            <body>
+                <h1>
+                Users ${users.length}
+                </h1>
+                <ul>
+                ${users.map(user =>{
+                    return(`
+                    <li>
+                    <div>
+                    email: ${user.email}
+                    </div>
+                    <div>
+                    bio: ${user.bio}
+                    </div>
+                    </li>
+                    `)
+                }).join('')}
+                </ul>
+            </body>
+            </html>
+        
+        
+        `);
     }
     catch(err){
         next(err);
